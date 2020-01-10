893ed38cd987e049128d533462f8cd57f9a4d63b
Jiawei Wang <jwewang@ucdavis.edu>
2020-01-10 12:19:15 -0800

    initial scripts

---
+++ run.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# checks if branch has something pending
+function parse_git_dirty() {
+    git diff --quiet --ignore-submodules HEAD 2>/dev/null; [ $? -eq 1 ] && echo "*"
+}
+
+# gets the current git branch
+function parse_git_branch() {
+    git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
+}
+
+# get last commit hash prepended with @ (i.e. @8a323d0)
+function parse_git_hash() {
+    git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/@\1/"
+}
+
+# DEMO
+echo "Git current branch and commit of the repository and submodules" > "git_branch_commit_info"
+
+GIT_BRANCH=$(parse_git_branch)$(parse_git_hash)
+echo "\njiawei-computation" >> "git_branch_commit_info"
+echo ${GIT_BRANCH} >> "git_branch_commit_info"
+
+cd jiawei-computational-results
+
+GIT_BRANCH=$(parse_git_branch)$(parse_git_hash)
+echo "\njiawei-computation-results" >> "../git_branch_commit_info"
+echo ${GIT_BRANCH} >> "../git_branch_commit_info"
+
+cd cutgeneratingfunctionology
+
+GIT_BRANCH=$(parse_git_branch)$(parse_git_hash)
+echo "\ncutgeneratingfunctionology" >> "../../git_branch_commit_info"
+echo ${GIT_BRANCH} >> "../../git_branch_commit_info"
+

69a8a3fa485e1ae93349a272622abd16bbb90e42
Jiawei Wang <jwewang@ucdavis.edu>
2020-03-31 19:41:37 -0700

    bug fix

--- regenerate_test_instances.sh
+++ regenerate_test_instances.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
 # get sage path to run sage directly
-source ./path.env
+#source ./path.env
 
 # clear test_instances folder
 if [[ -d ./test_instances ]]; then
-  rm -f ./test_instances/*
-else
-  mkdir ./test_instances/
+  rm -r ./test_instances/
 fi
 
+mkdir ./test_instances/
 mkdir ./test_instances/extreme_functions/
 mkdir ./test_instances/minimal_functions/
 mkdir ./test_instances/non_subadditive_functions/
 
 # generate test_cases
 echo 'The generation usually takes a couple of minutes.'
-sage ./scripts/generate_test_cases.sage
+#sage ./scripts/generate_test_cases.sage
